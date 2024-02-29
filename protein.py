b8b8525ed5c574616ba9bf6f98f4f5141855ef92
marcocarbullido <124408893+marcocarbullido@users.noreply.github.com>
2024-02-29 01:14:06 -0600

    Update protein.py

--- protein.py
+++ protein.py
@@ -12,5 +12,36 @@ class Protein():
   def get_sequence(self):
     sequence_dict = {}
     for line in self.lines:
-      sequence_dict.setdefault(line[5], line[3])
+      sequence_dict.setdefault(line[5], self.three_to_one(line[3]))
     return sequence_dict
+
+  def three_to_one(three_letter_code):
+    '''converts any three letter code to a one letter code'''
+    mapping = {'Ala':'A',
+              'Arg':'R',
+              'Asn':'N',
+              'Asp':'D',
+              'Cys':'C',
+              'Gln':'Q',
+              'Glu':'E',
+              'Gly':'G',
+              'His':'H',
+              'Ile':'I',
+              'Leu':'L',
+              'Lys':'K',
+              'Met':'M',
+              'Phe':'F',
+              'Pro':'P',
+              'Ser':'S',
+              'Thr':'T',
+              'Trp':'W',
+              'Tyr':'Y',
+              'Val':'V',}
+    
+    if len(three_letter_code) != 3: # What does this mean? Why are these residues denoted as AARG instead of ARG?
+      if three_letter_code[1:].lower() in [i.lower() for i in mapping.keys()]:
+        print(f"    WARNING: truncating {three_letter_code} to {three_letter_code[1:]}")
+        three_letter_code = three_letter_code[1:]
+      else:
+        print(f"    WARNING: {three_letter_code} was not found in function three_to_one map")
+    return mapping[three_letter_code[0].upper() + three_letter_code[1:].lower()]
